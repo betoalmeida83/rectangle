e71fce7aa6bb983cd33dc0418d9cff8cef5c583c
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-01-09 21:38:05 -0300

    Initial Commit

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,4 +1,30 @@
 package application;
 
+import entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = input.nextDouble();
+        rectangle.height = input.nextDouble();
+
+        rectangle.area();
+        rectangle.perimeter();
+        rectangle.diagonal();
+
+        System.out.println(rectangle);
+
+        input.close();
+
+    }
 }
